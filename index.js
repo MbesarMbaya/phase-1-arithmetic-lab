68f90d18f6e99ec40eaaf34b7f099d591daa3b40
Roy Mbaya <roymbaya08@gmail.com>
2022-11-30 19:08:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=31;
+let num2=2;
+const multiply=num1*num2
 
-// Write your code here
+const random=Math.floor(Math.random()*3)+2;
+
+let num3=30;
+let num4=13;
+const mod=num3%num4;
+
+const max=Math.max(14,15,20);
+
+
+
+    
